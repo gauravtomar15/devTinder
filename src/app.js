bc3c034499512375055f514644bad6f86927035a
Gaurav Tomar <gauravtomar@977gmail.com>
2025-10-29 02:44:26 +0530

    CRUD operation

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(express.json());
 
+// create the data
+
 app.post("/signUp", async(req , res)=>{
     
     const user = new User(req.body) ;
@@ -19,11 +21,63 @@ app.post("/signUp", async(req , res)=>{
    console.log("added the data")
 })
 
+// read the data
 
-connectDb().then(()=>{
+app.get("/user", async (req,res)=>{
+     const userId = req.body._id;
+ try {
+   console.log(userId);
+   const user = await User.findById({_id: userId});
+   if(!user){
+    res.send("user not found");
+   } else{
+   res.send(user);
+   }
+ } catch (error) {
+    res.status(404).send("somethig went wrong")
+ }
+});
+
+
+//delete the data
+
+app.delete("/user", async (req,res)=>{
+   const userId = req.body._id;
+   try{
+    console.log(userId);
+    const user = await User.findOneAndDelete(userId)
+    
+    res.send("user is deleted sucessfully");
+   }
+   catch (error) {
+   res.status(404).send("somethig went wrong")
+ }
+
+});
+
+
+// update the data
+
+app.patch("/user", async(req,res)=>{
+  const userId = req.body._id;
+  const Data = req.body;
+  try{
+   const user = await User.findByIdAndUpdate(userId, Data,{returnDocument: "after"});
+   console.log(user);
+   res.send("user updated sucessfully")
+  }
+   catch (error) {
+   res.status(404).send("somethig went wrong")
+ }
+});
+
+
+connectDb()
+.then(()=>{
     console.log("db is connected");
     app.listen(7777,()=>{console.log("server is ready")});
-}).catch((err)=>{
+  })
+.catch((err)=>{
     console.error("db is not connected")
 });
 
